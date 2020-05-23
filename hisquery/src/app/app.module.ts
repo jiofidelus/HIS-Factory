87e9fbffae7be010ccd0761957fb5db2c932c59b
lizzaraga <fomekongchristmael@gmail.com>
2020-05-23 13:02:56 +0100

    Workspace page created

--- hisquery/src/app/app.module.ts
+++ hisquery/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MaterialModule } from './material.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginModalComponent } from './components/login-modal/login-modal.component';
 import { RegisterModalComponent } from './components/register-modal/register-modal.component';
+import { WorkspacePageComponent } from './pages/workspace-page/workspace-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     LoginModalComponent,
-    RegisterModalComponent
+    RegisterModalComponent,
+    WorkspacePageComponent
   ],
   imports: [
     BrowserModule,
